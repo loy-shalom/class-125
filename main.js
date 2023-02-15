51efc44d3e6fb57f13c899402c829f6ab9b35d83
loy-shalom <93376919+loy-shalom@users.noreply.github.com>
2023-02-15 17:51:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+
+    canvas = createCanvas(550, 500);
+    canvas.position(560,150);
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Intialized!');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("noseX = " + noseX +" noseY = " + noseY);
+
+        leftWristx = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightwrist.x;
+        difference = leftwrist - rightwrist;
+
+    }
+}
+
+function draw() {
+    background('#969A97');
+
+    document.getElementById("square_side").innerHTML = "width and height of a square will be = " +difference +"px";
+    fill('#F90093');
+    stroke('#F90093');
+    square(noseX, noseY, difference);
+}
